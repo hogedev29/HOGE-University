d141da079b2f276c1229e42680dddae7cca0b434
hoge.dev29 <hogedev29@gmail.com>
2021-03-13 19:23:06 +0000

    Build side menu dinamically. Add website content.

---
+++ src/components/links.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import "./style.scss";
+
+const Links = ({ links }) => (
+  <ul className="links-list ml-5">
+    {links.map((data) => {
+      return (
+        <li>
+          <div className="box">
+            <article className="media center">
+              <figure className="media-left">
+                <span className="icon is-large">
+                  <img src={data.image} />
+                </span>
+              </figure>
+              <div className="media-content">
+                <div className="content">
+                  <a target="_blank" rel="noreferrer" href={data.url}>
+                    {data.title}
+                  </a>
+                  <p className="subtitle is-size-5">{data.subtitle}</p>
+                </div>
+              </div>
+            </article>
+          </div>
+        </li>
+      );
+    })}
+  </ul>
+);
+
+export default Links;
---
+++ src/content/side-menu.js
@@ -0,0 +1,42 @@
+export default {
+  sections: [
+    {
+      title: "General",
+      children: [
+        {
+          title: "What is HOGE?",
+          slug: "/what-is-hoge",
+        },
+        {
+          title: "How can i buy HOGE?",
+          slug: "/how-to-buy",
+        },
+      ],
+    },
+
+    {
+      title: "Crypto School",
+      children: [
+        {
+          title: "Learning Videos",
+          slug: "/learning-videos",
+        },
+      ],
+    },
+
+    {
+      title: "Links",
+      children: [
+        {
+          title: "Offical Websites",
+          slug: "/official-websites",
+        },
+
+        {
+          title: "Community Websites",
+          slug: "/community-websites",
+        },
+      ],
+    },
+  ],
+};
