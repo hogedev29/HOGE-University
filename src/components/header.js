2c7e6431002d1e82fdb90da6d839c82174f23eab
hoge.dev29 <hogedev29@gmail.com>
2021-03-13 20:28:12 +0000

    Change header style. Add top header menu.

--- src/components/header.js
+++ src/components/header.js
@@ -4,9 +4,25 @@ import "./style.scss";
 
 import hogeLogo from "../images/hoge-logo.png";
 
+const UnderlineLink = ({ url, title }) => (
+  <div className="underline-link">
+    <a rel="noreferrer" target="_blank" href={url}>
+      {title}
+    </a>
+    <div className="line" />
+  </div>
+);
+
 const Header = ({ siteTitle }) => (
   <section className="hero heroBg is-small">
     <div className="hero-body">
+      <div className="top-header center mb-6">
+        <UnderlineLink url="https://www.hoge.finance" title="HOGE WEBSITE" />
+        <UnderlineLink url="https://www.hogefinance.shop" title="HOGE SHOP" />
+        <UnderlineLink url="https://www.hoge.fun" title="HOGE FUN" />
+        <UnderlineLink url="https://www.hogemint.com" title="HOGE MINT" />
+      </div>
+
       <div className="header container center">
         <article className="media center">
           <figure className="icon logo media-left">
