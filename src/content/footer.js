85ee6d792c7088b5a9736eaf9bfcc6f4f72fb6d7
hoge.dev29 <hoge.dev29@gmail>
2021-03-15 11:10:41 +0000

    Update footer content.

---
+++ src/content/footer.js
@@ -0,0 +1,82 @@
+export default {
+  sections: [
+    {
+      title: "Social",
+      children: [
+        {
+          title: "Twitter",
+          url: "https://twitter.com/hogefinance",
+        },
+        {
+          title: "Youtube",
+          url: "https://www.youtube.com/channel/UCB1sNAdsnBTYK_gpnWxFquA",
+        },
+        {
+          title: "Facebook",
+          url: "https://www.facebook.com/groups/269850121315355",
+        },
+        {
+          title: "Instagram",
+          url: "https://www.instagram.com/hogeofficial/",
+        },
+        {
+          title: "Telegram",
+          url: "https://t.co/WX8kiUaUGZ?amp=1",
+        },
+        {
+          title: "Discord",
+          url: "https://discord.gg/hoge",
+        },
+      ],
+    },
+    {
+      title: "HOGE Official",
+      children: [
+        {
+          title: "HOGE Websites",
+          url: "https://hoge.finance/",
+        },
+        {
+          title: "HOGE Shop",
+          url: "https://hogefinance.shop/",
+        },
+        {
+          title: "HOGE Fun",
+          url: "https://hoge.fun/",
+        },
+        {
+          title: "HOGE Mint",
+          url: "https://hogemint.com/",
+        },
+        {
+          title: "HOGE Donate",
+          url: "https://hoge.fun/donate",
+        },
+      ],
+    },
+    {
+      title: "Development",
+      children: [
+        {
+          title: "GitHub",
+          url: "https://github.com/HogeFinance",
+        },
+        {
+          title: "Etherscan",
+          url:
+            "https://etherscan.io/token/0xfad45e47083e4607302aa43c65fb3106f1cd7607",
+        },
+        {
+          title: "Dextools",
+          url:
+            "https://www.dextools.io/app/uniswap/pair-explorer/0x7fd1de95fc975fbbd8be260525758549ec477960",
+        },
+        {
+          title: "Ethplorer",
+          url:
+            "https://ethplorer.io/address/0xfad45e47083e4607302aa43c65fb3106f1cd7607#chart=candlestick",
+        },
+      ],
+    },
+  ],
+};
