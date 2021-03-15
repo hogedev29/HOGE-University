097e0fd43872ffd918289f54eec59033c9fed07d
hoge.dev29 <hogedev29@gmail.com>
2021-03-15 14:31:11 +0000

    Extract notification component

---
+++ src/components/notification.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Message } from "react-bulma-components";
+
+const Notification = () => (
+  <div className="container toast mt-4">
+    <section>
+      <Message color="danger has-text-centered">
+        <Message.Body>
+          Help developers. <strong>HOGE developers</strong>, need your help!
+        </Message.Body>
+      </Message>
+    </section>
+  </div>
+);
+
+export default Notification;
