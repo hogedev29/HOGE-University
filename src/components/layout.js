e4d0bbed54f174f48e93b713f61279af90675260
hoge.dev29 <hoge.dev29@gmail>
2021-03-15 14:31:11 +0000

    Extract notification component

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,19 +5,8 @@ import Helmet from "./helmet";
 import Header from "./header";
 import Midsection from "./midsection";
 import Footer from "./footer";
-import { Message } from "react-bulma-components";
+import Notification from "./notification";
 
-const Notification = () => (
-  <div className="container toast mt-4">
-    <section>
-      <Message color="danger has-text-centered">
-        <Message.Body>
-          Help developers. <strong>HOGE developers</strong>, need your help!
-        </Message.Body>
-      </Message>
-    </section>
-  </div>
-);
 const Layout = ({ children }) => (
   <div>
     <Helmet />
