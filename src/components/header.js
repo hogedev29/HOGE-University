6f6fb58085eaeb28a74744b919348b263c6a2d0e
hoge.dev29 <hogedev29@gmail.com>
2021-03-15 12:02:46 +0000

    Updated header content

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,10 @@ import React from "react";
 import "./style.scss";
 
 import hogeLogo from "../images/hoge-logo.png";
+import coinMarketcap from "../images/coinMarketcap.png";
+import coinbase from "../images/coinbase.png";
+import whitebit from "../images/whitebit.png";
+import blockfolio from "../images/blockfolio.svg";
 
 export const UnderlineLink = ({ url, title }) => (
   <div className="underline-link">
@@ -24,15 +28,53 @@ const Header = ({ siteTitle }) => (
         <UnderlineLink url="https://hoge.fun/donate" title="HOGE DONATE" />
       </div>
 
-      <div className="header container center">
-        <article className="media center">
+      <div className="header container">
+        <div className="center mb-6">
           <figure className="icon logo media-left">
             <img src={hogeLogo} alt="hoge-logo" />
           </figure>
-          <div className="media-content">
-            <h1 className="title">HOGE.University</h1>
+          <h1 className="title">HOGE.University</h1>
+        </div>
+        <div className="exchanges has-text-centered center columns">
+          <div className="column" />
+          <div className="column">
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="https://coinmarketcap.com/currencies/hoge-finance/"
+            >
+              <img src={coinMarketcap}></img>
+            </a>
           </div>
-        </article>
+          <div className="column">
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="https://www.coinbase.com/price/hoge-finance"
+            >
+              <img src={coinbase}></img>
+            </a>
+          </div>
+          <div className="column">
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="https://whitebit.com/trade/HOGE_USDT"
+            >
+              <img src={whitebit}></img>
+            </a>
+          </div>
+          <div className="column">
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="https://blockfolio.com/coin/HOGE"
+            >
+              <img src={blockfolio}></img>
+            </a>
+          </div>
+          <div className="column" />
+        </div>
       </div>
     </div>
   </section>
