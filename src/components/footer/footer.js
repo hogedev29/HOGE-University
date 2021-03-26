7ad3504eb711d62464c89ccbcbe9852ee39fed22
hoge.dev29 <hogedev29@gmail.com>
2021-03-26 19:52:07 +0000

    Upate footer UI

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Heading } from "react-bulma-components";
+import { Heading, Container } from "react-bulma-components";
 import { StaticQuery, graphql } from "gatsby";
 
 const FooterMobile = () => (
@@ -30,21 +30,26 @@ const FooterWide = () => (
     `}
     render={(data) => (
       <footer className="footer footer-wide">
-        <div className="container">
-          <div className="footer-hero mb-6">
-            <Heading className="center">Explore more of HOGE!</Heading>
-            <p className="center">Can't get enough? We understand!</p>
-          </div>
+        <div className="footer-hero mb-6">
+          <h1 className="title center">Explore more of HOGE!</h1>
+          <h2 className="subtitle center">Can't get enough? We understand.</h2>
+        </div>
 
-          <div className="columns">
-            {data.allFooterJson.nodes.map((el) => {
-              return (
-                <div className="column has-text-centered">
-                  <FooterColumn title={el.title} items={el.items} />
-                </div>
-              );
-            })}
-          </div>
+        <div className="footer-wrapper">
+          <Container>
+            <div className="columns">
+              {data.allFooterJson.nodes.map((el) => {
+                return (
+                  <div className="column footer-column">
+                    <FooterColumn title={el.title} items={el.items} />
+                  </div>
+                );
+              })}
+            </div>
+          </Container>
+        </div>
+        <div className="footer-bottom has-text-centered mt-5">
+          <span>Developed by a member of HOGE community</span>
         </div>
       </footer>
     )}
@@ -53,15 +58,18 @@ const FooterWide = () => (
 
 const FooterColumn = ({ title, items }) => (
   <div>
-    <Heading className="footer-heading" size={4}>
-      {title}
-    </Heading>
-    <ul className="footer-list">
+    <h5 className="footer-column-title">{title}</h5>
+    <ul className="footer-column-list">
       {items &&
         items.map((el) => {
           return (
             <li>
-              <a rel="noreferrer" target="_blank" href={el.url}>
+              <a
+                className="btn-hover-underline-white"
+                rel="noreferrer"
+                target="_blank"
+                href={el.url}
+              >
                 {el.title}
               </a>
             </li>
